8f042f7a02b6e6036c02cdc55aef8648199de6d2
leigh966 <leigh966@hotmail.com>
2022-08-15 23:47:52 +0100

    Made Game check if game_id is valid by asking the backend for the corresponding drawDate and outputting it. If a drawDate corresponding to the id is not found, the user is redirected to a 404 screen.

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,3 +1,24 @@
+import { BACKEND_URL } from "../webconfig";
+import NoPage from "./NoPage";
+import { root } from "../Root";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+
+function getGameDrawDate(gameId, setDrawDate) {
+  fetch(`${BACKEND_URL}/game/${gameId}/draw_date`)
+    .then((response) => {
+      if (response.status === 200) return response.text();
+      else root.render(<NoPage />);
+    })
+    .then(setDrawDate)
+    .catch(console.log);
+}
+
 export default function Game() {
-  return "Game Here";
+  let { id } = useParams();
+  let [drawDate, setDrawDate] = useState("");
+  if (drawDate === "") {
+    getGameDrawDate(id, setDrawDate);
+  }
+  return <h1>{drawDate}</h1>;
 }
