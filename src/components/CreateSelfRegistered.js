19844342c3904d9a43f335083014258e4d28f312
leigh966 <leigh966@hotmail.com>
2022-11-11 15:00:12 +0000

    Fixed creation with groups

--- src/components/CreateSelfRegistered.js
+++ src/components/CreateSelfRegistered.js
@@ -3,13 +3,18 @@ import { BACKEND_URL, JSON_HEADERS, STATUS } from "../webconfig";
 import GameCreated from "../pages/GameCreated";
 
 async function postNewGame(props) {
+  let groupsString = "[";
+  props.groupNames.forEach((element) => {
+    groupsString += `"${element}",`;
+  });
+  groupsString = groupsString.substring(0, groupsString.length - 1) + "]";
   let game_id = -1;
   const options = {
     method: "POST",
     headers: JSON_HEADERS,
     body: `{
        "draw": "${props.drawDate}",
-       "groups": "${props.groupNames}"
+       "groups": ${groupsString}
       }`,
   };
   await fetch(BACKEND_URL + "create-session/self-register", options)
