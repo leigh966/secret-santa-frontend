leigh966/secret-santa-frontend

commit 92ff021c4b61ddd0ac076d7a2e97d6fb691abf4d

Fixed the placeholder message for the group dropdown and added a heading to reflect whether the user is logging on or registering

src/components/SelfRegister.js
@@ -57,15 +57,37 @@ function RegisterButton(props) {
     return <button onClick={() => props.setRegistering(true)}>Register</button>;
 }
 
+function getGroupDropdown(
+  dropDownSelection,
+  setDropdown,
+  game_id,
+  registering
+) {
+  if (registering)
+    return (
+      <GroupDropdown
+        current={dropDownSelection}
+        handler={setDropdown}
+        gameId={game_id}
+      />
+    );
+}
+
+function getHeader(registering) {
+  if (registering) return <h1>Register</h1>;
+  else return <h1>Log In</h1>;
+}
+
 export default function SelfRegister(props) {
   game_id = props.gameId;
   let [name, setName] = useState("");
   let [password, setPassword] = useState("");
   let [registering, setRegistering] = useState(false);
-  let [dropDownSelection, setDropdown] = useState({ value: null, label: null });
+  let [dropDownSelection, setDropdown] = useState(""); // needs to be falsy for placeholder to show
   console.log(dropDownSelection);
   return (
     <div>
+      {getHeader(registering)}
       <NamePasswordForm
         name={name}
         password={password}
@@ -79,11 +101,7 @@ export default function SelfRegister(props) {
           }
         }}
       />
-      <GroupDropdown
-        current={dropDownSelection}
-        handler={setDropdown}
-        gameId={game_id}
-      />
+      {getGroupDropdown(dropDownSelection, setDropdown, game_id, registering)}
       <RegisterButton
         registering={registering}
         setRegistering={setRegistering}

src/components/GroupDropdown.js
@@ -47,7 +47,7 @@ export default class GroupDropdown extends React.Component {
             this.props.handler(option);
           }}
           value={this.props.current}
-          placeholder="Select a group"
+          placeholder={"Select a group"}
         />
       );
   }

commit 89b5713e5481799e74e05719a721aec6afd22ed8

Fixed groupless registration that was broke by commit 92ff021c4b61ddd0ac076d7a2e97d6fb691abf4d

src/components/SelfRegister.js
@@ -83,7 +83,7 @@ export default function SelfRegister(props) {
   let [name, setName] = useState("");
   let [password, setPassword] = useState("");
   let [registering, setRegistering] = useState(false);
-  let [dropDownSelection, setDropdown] = useState(""); // needs to be falsy for placeholder to show
+  let [dropDownSelection, setDropdown] = useState(null); // needs to be falsy for placeholder to show
   console.log(dropDownSelection);
   return (
     <div>
@@ -95,7 +95,11 @@ export default function SelfRegister(props) {
         setPassword={setPassword}
         onSubmit={() => {
           if (registering) {
-            tryRegister(name, password, dropDownSelection.value);
+            tryRegister(
+              name,
+              password,
+              dropDownSelection ? dropDownSelection.value : null
+            );
           } else {
             tryLogin(name, password);
           }