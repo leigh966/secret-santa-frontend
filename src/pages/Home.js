1fa96076258499c35abe4d82cb1b8bbc26165d87
leigh966 <leigh966@hotmail.com>
2022-11-23 14:07:46 +0000

    Updating version number for deployment

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ function startCreatingGame() {
 export default function Home() {
   return (
     <div className="Home">
-      v1.0.4
+      v1.0.5
       <Helmet>
         <link rel="stylesheet" href="Home.css" />
       </Helmet>
