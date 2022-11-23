c4207ed8f53f1e285be567db62ec6eba3a2bd298
leigh966 <leigh966@hotmail.com>
2022-11-23 16:29:54 +0000

    Updated deploy script so that it doesn't keep resetting the deploy domain

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ function startCreatingGame() {
 export default function Home() {
   return (
     <div className="Home">
-      v1.1.0
+      v1.1.1
       <Helmet>
         <link rel="stylesheet" href="Home.css" />
       </Helmet>
