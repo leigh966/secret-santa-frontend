a17277661f873f45b585fa11d2dcc560c97f8159
leigh966 <leigh966@hotmail.com>
2022-11-23 16:28:29 +0000

    Removed debug features as main is now to be considered a production branch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ function startCreatingGame() {
 export default function Home() {
   return (
     <div className="Home">
-      v1.0.7
+      v1.1.0
       <Helmet>
         <link rel="stylesheet" href="Home.css" />
       </Helmet>
